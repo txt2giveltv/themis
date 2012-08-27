6ef74f25eb6aeb7e55a94c5684d89275624187a0
Potapov Sergey <blake131313@gmail.com>
2012-08-27 20:09:06 +0300

    no restriction for "validate" method
    Task 5441

--- lib/themis/validation.rb
+++ lib/themis/validation.rb
@@ -36,7 +36,7 @@ module Themis
 
     # Save all calls of validation methods as array of validators
     def method_missing(method_name, *args)
-      if method_name.to_s =~ /\Avalidates/
+      if method_name.to_s =~ /\Avalidate/
         self.validators << Themis::Validation::Validator.new(method_name, args)
       else
         super
